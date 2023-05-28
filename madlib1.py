a738f5b34535223e72446216a9b67eceea7af2f6
YaraTejaswini <134850706+YaraTejaswini@users.noreply.github.com>
2023-05-28 22:24:03 +0530

    Add files via upload

---
+++ madlib1.py
@@ -0,0 +1,25 @@
+def madl():
+    verb=input("verb:")
+    adj=input("adjective:")
+    adj1=input("adjective1:")
+    adj2=input("adjective2:")
+    adj3=input("adjective3:")
+    noun=input("noun:")
+    noun1=input("noun1:")
+    
+    
+    
+    
+    madl= f"I love computer programming because its {adj}! The journey to becoming a good programmer \
+starts with hope in your mind and dreams. Yes, once you learn to code {verb},it becomes a part \
+of your life identity and you become a great {adj1} coder.Knowledge of programming lets you \
+take control your {noun}.You can creat your own personal {noun1}, anything from developing {adj2}\
+software to analyzing data and making predictions about the {noun1}.I hope you'll start your {adj3}\
+journey by next moment."
+    
+    
+    print(madl)
+    
+    
+    
+    
---
+++ madlibs.py
@@ -0,0 +1,6 @@
+from Sample_madlibs import madlib1,madlib2
+import random
+
+if __name__ == "__main__":
+    m = random.choice([madlib1,madlib2])
+    m.madl()
